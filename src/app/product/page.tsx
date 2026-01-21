2e5878121f7ae12d2c9286b7176939f3b2708be1
wenza007 <overkung4399@gmail.com>
2026-01-21 23:41:52 +0700

    edit /products instread /uploads in imagesrc

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -209,7 +209,7 @@ export default function ProductList() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {filtered.map(p => {
             const imageSrc = p.imageUrl
-              ? `${process.env.NEXT_PUBLIC_API_URL}/uploads/${p.imageUrl}`
+              ? `${process.env.NEXT_PUBLIC_API_URL}/products/${p.imageUrl}`
               : null;
 
             return (
